5cd5426155959fe9234fc418295d9ab1cf882475
Jon Mockbee <mockbee1000@gmail.com>
2019-07-17 09:30:38 -0400

    making sure it works

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+
+    public static void main(String[] args) {
+      int newScore =  CalculateScore ("jon", 500);
+        System.out.println("New score is " +newScore);
+    }
+
+    public static int CalculateScore( String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+
+    }
+
 }
