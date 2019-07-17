7bf7c6ce4e8c29dc738daa9b13cc031433d355a0
Jon Mockbee <mockbee1000@gmail.com>
2019-07-17 09:49:57 -0400

    adding more methods

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,24 @@ public class Main {
     public static void main(String[] args) {
       int newScore =  CalculateScore ("jon", 500);
         System.out.println("New score is " +newScore);
+        CalculateScore(75);
+        CalculateScore();
     }
 
     public static int CalculateScore( String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points");
+        // this below returns the score back up to int newScore
         return score * 1000;
 
     }
 
+    public static int CalculateScore(  int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int CalculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
 }
